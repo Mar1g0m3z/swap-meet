05eea86a90fb966e3bf996f0a843b541d9c61045
marisela <mariselaalejandragomez@gmail.com>
2025-03-31 11:54:30 -0700

    Finished wave 2. Create Item class with requirements. Attempted to remake a vendor class with the needed function in items.py but found that I just needed to update the vendor.py with the new function for wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,3 +14,9 @@ class Vendor:
             self.inventory.remove(item)
             return item
         return False
+
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None
